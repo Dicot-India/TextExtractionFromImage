f4096c5d554571c0a751c948f779de32b2eb0023
Divyesh Prajapati <divyesh220998@gmail.com>
2025-02-21 10:19:08 +0530

    Added backend path in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://15.207.232.194:9999/:path*", // Replace with actual backend URL
+            },
+        ];
+    },
+};
 
 export default nextConfig;
