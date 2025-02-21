d3b782ea80a00687e7b6273d6cece301f3de4e2f
Divyesh Prajapati <divyesh220998@gmail.com>
2025-02-21 14:14:40 +0530

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/:path*",
+                source: "*",
                 destination: "http://15.207.232.194:9999/:path*", // Replace with actual backend URL
             },
         ];
