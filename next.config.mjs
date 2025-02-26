b0b393e77484aa158bcbf216d91378d806b7cbb4
Divyesh Prajapati <divyesh220998@gmail.com>
2025-02-26 11:41:00 +0530

    Added proxy.js file

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/:path*",
-                destination: "http://15.207.232.194:9999/:path*", // Replace with actual backend URL
+                destination: "/:path*", // Replace with actual backend URL
             },
         ];
     },
