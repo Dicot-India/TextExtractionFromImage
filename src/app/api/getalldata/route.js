8116de0650a4d8ba7506d4a1d927c3628265ae7e
Divyesh Prajapati <divyesh220998@gmail.com>
2025-02-26 15:55:08 +0530

    Change in method

--- src/app/api/getalldata/route.js
+++ src/app/api/getalldata/route.js
@@ -2,15 +2,11 @@ export async function GET(request, response) {
     const backendUrl = `http://15.207.232.194:9999${request.nextUrl.pathname.replace('/api/getalldata', '/get_all_data')}`;
 
     try {
-
-        const body = await request.json();
         const response = await fetch(backendUrl, {
-            method: "POST",
+            method: "GET",
             headers: {
                 "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify(body),
+            }
         });
         const data = await response.json();
         return Response.json(data, { status: response.status });
